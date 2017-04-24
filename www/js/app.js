b8c6b2c01ea535ace331083f2940ec806a3d81ab
mssaunders <mssaunders@wisc.edu>
2017-04-23 21:16:45 -0500

    Initial Commit

---
+++ www/js/app.js
@@ -0,0 +1,38 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var starter = angular.module('starter', ['ionic', 'ngCordova'])
+
+  .run(function($ionicPlatform, $cordovaSQLite) {
+    $ionicPlatform.ready(function() {
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+      window.connectManager = new connectsdk.ConnectManager();
+      window.connectManager.init();
+    });
+  })
+
+// The start of the single controller that we will be using for this lab
+// It is called "mainCtrl" and is connected to the Angular module "starter"
+starter.controller('mainCtrl', function($scope, $ionicModal, $ionicLoading) {
+
+  $scope.onSubmit = function () { // Create onSubmit function
+
+    $ionicLoading.show({ template: 'Person Added!', noBackdrop: true, duration: 1000 });
+
+  };
+
+})
